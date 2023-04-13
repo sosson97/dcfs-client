f9626922767a56746c904ba23649e12401af5a53
Sam Son <sosson97@gmail.com>
2023-04-13 08:12:47 +0000

    remove old files

--- src/errno.hpp
+++
@@ -1,9 +0,0 @@
-using err_t = int;
-
-#define NO_ERR 0
-#define ERR_NOT_FOUND -1
-#define ERR_NO_SPACE -2
-#define ERR_NO_CONN -3
-#define ERR_HASH -4
-#define ERR_IO -5
-#define ERR_BUF_TOO_SMALL -6
